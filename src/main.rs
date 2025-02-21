e287b25173befd8f7913d34dcfc7b20d5bf60504
Nic Coxen <nicjcoxen@gmail.com>
2025-02-22 00:08:45 +1300

    Simple List

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    //  empty vector
+    let mut list: Vec<i32> = Vec::new();
+
+    // elements to vector
+    list.push(1);
+    list.push(2);
+    list.push(3);
+
+    // print
+    for element in &list {
+        println!("{}", element);
+    }
 }
